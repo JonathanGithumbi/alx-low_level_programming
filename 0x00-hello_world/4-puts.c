0da63a1115d647f51da00599f046407d161c3b26
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-16 00:56:56 -0800

    Mod 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/*
+ *The main function printrs a string using puts and returns 
+ *Return: Always 0 (Success)
+ * */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
