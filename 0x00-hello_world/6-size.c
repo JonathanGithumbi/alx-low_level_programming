1ac898a9bfbeebf7d7d1ed85be4b53c59e329a32
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-16 08:54:34 -0800

    Betty m,od 1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-	uintptr_t bits = 8 * sizeof(void*);
+	uintptr_t bits = 8 * sizeof(void *);
+
 	if (bits == 64)
 	{
 		printf("Size of a char: 1 byte(s)\n");
@@ -25,4 +26,5 @@ int main(void)
                 printf("Size of a long long int: 8 byte(s)\n");
                 printf("Size of a float: 4 byte(s)\n");
 	}
+	return (0);
 }
