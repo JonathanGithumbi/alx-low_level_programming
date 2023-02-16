e03511426ae93f9f6a1b8dc097c86d2310824cab
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-16 09:09:52 -0800

    6-size betty spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,10 +21,10 @@ int main(void)
 	if (bits == 32)
 	{
 		printf("Size of a char: 1 byte(s)\n");
-                printf("Size of an int: 4 byte(s)\n");
-                printf("Size of a long int: 4 byte(s)\n");
-                printf("Size of a long long int: 8 byte(s)\n");
-                printf("Size of a float: 4 byte(s)\n");
+		printf("Size of an int: 4 byte(s)\n");
+		printf("Size of a long int: 4 byte(s)\n");
+		printf("Size of a long long int: 8 byte(s)\n");
+		printf("Size of a float: 4 byte(s)\n");
 	}
 	return (0);
 }
