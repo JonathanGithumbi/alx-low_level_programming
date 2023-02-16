e06933dcfa5b180c88199436a2da56b7c9fbf737
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-16 01:01:51 -0800

    betty style and documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/*
- *The main function printrs a string using puts and returns 
- *Return: Always 0 (Success)
- * */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success) 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
