62a29a8fc867b9fc6ada2b08824c8d2ecae68d4a
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-16 08:46:55 -0800

    Mod size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 /**
  * main - Entry point
@@ -7,10 +8,21 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n");
-	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
-	return 0;
+	uintptr_t bits = 8 * sizeof(void*);
+	if (bits == 64)
+	{
+		printf("Size of a char: 1 byte(s)\n");
+		printf("Size of an int: 4 byte(s)\n");
+		printf("Size of a long int: 8 byte(s)\n");
+		printf("Size of a long long int: 8 byte(s)\n");
+		printf("Size of a float: 4 byte(s)\n");
+	}
+	if (bits == 32)
+	{
+		printf("Size of a char: 1 byte(s)\n");
+                printf("Size of an int: 4 byte(s)\n");
+                printf("Size of a long int: 4 byte(s)\n");
+                printf("Size of a long long int: 8 byte(s)\n");
+                printf("Size of a float: 4 byte(s)\n");
+	}
 }
