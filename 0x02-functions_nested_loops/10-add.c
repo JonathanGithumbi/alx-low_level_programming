25338c654013faf2ec92c99fd4f8a32d44af3b43
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-21 13:01:10 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ *@a: the first number to be added
+ *@b: the second number to be added
+ * Return: Returns the total
+ */
+int add(int a, int b)
+{
+	int total;
+
+	total = a + b;
+	return (total);
+}
