0378a8f2b563cc0690fdb6ff7ccb4bd4302e185f
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-21 11:05:05 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an iteger
+ *
+ *
+ * @num: number to be checked
+ * Return: abs
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
+
