d328f0c2096e1d1dcf7e5883c8c2ed11d623051e
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-23 18:36:33 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,37 +3,35 @@
 #include <math.h>
 
 /**
- * largest_prime_factor - finds the largest prime factor of a number
- * @n: the number to find the largest prime factor of
- *
- * Return: the largest prime factor of n
+ * largest_prime_factor - Finds the lasrgest prime factore
+ * @n: the number to find the largest prime factor ot
+ * Return: the largest prime factor
  */
 long largest_prime_factor(long n)
 {
-	long i, max;
+	long i;
+	long max;
 
-	/* divide by 2 until n is no longer even */
-   	while (n % 2 == 0)
-   	{
-   		max = 2;
-        	n /= 2;
-    	}
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
 
-    	/* check odd prime factors up to the square root of n */
-    	for (i = 3; i <= sqrt(n); i += 2)
-    	{
-        	while (n % i == 0)
-        	{
-            		max = i;
-            		n /= i;
-        	}
-    	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n /= i;
+		}
+	}
 
-    	/* if n is still greater than 2, it must be prime */
-    	if (n > 2)
-        	max = n;
-
-    	return (max);
+	if (n > 2)
+	{
+		max = n;
+	}
+	return (max);
 }
 
 /**
@@ -43,11 +41,9 @@ long largest_prime_factor(long n)
  */
 int main(void)
 {
-    long n = 612852475143;
-    long result = largest_prime_factor(n);
+	long n = 612852475143;
+	long result = largest_prime_factor(n);
 
-    printf("%ld\n", result);
-
-    return (0);
+	printf("%ld\n", result);
+	return (0);
 }
-
