89913b56f43ac13336d6040593ad5f03cfe11c72
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-02-27 14:51:12 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of  string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be reversed
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = _strlen(s); len >= 0; len--)
+	{
+		_putchar(*(s + len));
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_rev(char *s);
 void swap_int(int *a, int *b);
 int _putchar(char c);
 void reset_to_98(int *n);
