943f68c3a38311fdcde7e9fe3600af96e245063a
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-03-02 10:05:25 -0800

    capstring

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all workds of a string
+ * @str: the string to be modified
+ * Return: a pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+            str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+            str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+            str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+	}
+	return (str);
+}
