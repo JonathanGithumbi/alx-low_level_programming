f80713d250382ef4b5ad1a5b5af4db85011f1eb8
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-03-08 08:45:01 -0800

    mod

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _preint_rev_recursion - Prints a string in reverse
- * 
- *@s: the string to print in reverse
+ * _print_rev_recursion - Prints a string in reverse
+ *
+ * @s: the string to print in reverse
  */
 void _print_rev_recursion(char *s)
 {
