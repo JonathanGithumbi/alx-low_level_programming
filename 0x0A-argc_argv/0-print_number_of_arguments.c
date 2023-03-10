9c76d4cc00c62d4670648db0ef02cd378434d8d1
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-03-10 07:51:56 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-print_number_of_arguments.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * print_number_of_arguments - prints the number oof arguments 
+ * @argc: 
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ *
+ * @argc: the number of command line arguments
+ * @argv: an array of commandline argument strings
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+	{
+		(void)argc;
+		printf("%s\n", argv[0]);
+
+		return (0);
+	}
